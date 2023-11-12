8b06703d00c3467df0efaf21e2238b882f6c0dd8
ercanunal4406 <ercanunal4406@hotmail.com.tr>
2023-11-12 10:47:24 +0300

    First Commit

---
+++ Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenemeProje.Models
+{
+    public class Person
+    {
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+
+        List<Person> people = new List<Person>();
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using DenemeProje.Models;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>();
+        {
+            new Person()
+            {
+                FirstName = "Sina Emre";
+            LastName = "BEKAR";
+        },
+         new Person()
+         {
+             FirstName = "Arda";
+        LastName = "ŞİŞMAN";
+    },
+     new Person()
+    {
+        FirstName = "dOĞUKAN ";
+        LastName = "YALÇIN";
+    },
+     new Person()
+    {
+        FirstName = "Ercan";
+        LastName = "ÜNAL";
+    },
+    };
+Console.WriteLine("Sınıf Listesi==========");
+}
+
